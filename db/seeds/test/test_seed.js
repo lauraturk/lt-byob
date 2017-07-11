f9cf47395a6a4f38e9a79891651b642c386fe32a
Laura Turk <laura.lk.turk@gmail.com>
2017-07-10 21:05:45 -0600

    Configure test env and write happy path get tests

---
+++ db/seeds/test/test_seed.js
@@ -0,0 +1,89 @@
+/*jshint esversion: 6 */
+
+const data = require('../../../data/cleanData');
+
+  const createTextSample = (knex, text_sample) => {
+    return knex('text_samples').insert({
+      title: text_sample.title,
+      body: text_sample.body
+    }, 'id')
+    .then(text_sampleId => {
+      let posPromises = [];
+
+      text_sample.adjectives.forEach(adjective => {
+        posPromises.push(
+          createAdjective(knex, {
+            word: adjective.word,
+            type: adjective.type,
+            text_id: text_sampleId[0]
+          })
+        );
+      });
+
+      text_sample.nouns.forEach(noun => {
+        posPromises.push(
+          createNoun(knex, {
+            word: noun.word,
+            type: noun.type,
+            text_id: text_sampleId[0]
+          })
+        );
+      });
+
+      text_sample.verbs.forEach(verb => {
+        posPromises.push(
+          createVerb(knex, {
+            word: verb.word,
+            type: verb.type,
+            text_id: text_sampleId[0]
+          })
+        );
+      });
+
+      text_sample.adverbs.forEach(adverb => {
+        posPromises.push(
+          createAdverb(knex, {
+            word: adverb.word,
+            type: adverb.type,
+            text_id: text_sampleId[0]
+          })
+        );
+      });
+
+      return Promise.all(posPromises);
+    });
+  };
+
+  const createAdjective = (knex, adjective) => {
+    return knex('adjectives').insert(adjective);
+  };
+
+  const createNoun = (knex, noun) => {
+    return knex('nouns').insert(noun);
+  };
+
+  const createVerb = (knex, verb) => {
+    return knex('verbs').insert(verb);
+  };
+
+  const createAdverb = (knex, adverb) => {
+    return knex('adverbs').insert(adverb);
+  };
+  // Deletes ALL existing entries
+exports.seed = (knex, Promise) => {
+  return knex('verbs').del()
+    .then(() => knex('adjectives').del())
+    .then(() => knex('nouns').del())
+    .then(() => knex('adverbs').del())
+    .then(() => knex('text_samples').del())
+    .then(() => {
+      let textSamplePromises = [];
+
+      data.text_samples.forEach(text_sample => {
+        textSamplePromises.push(createTextSample(knex, text_sample));
+      });
+
+      return Promise.all(textSamplePromises);
+    })
+  .catch(error => console.log(`Error seeding data ${error}`));
+};
---
+++ test/routes.spec.js
@@ -0,0 +1,98 @@
+/*jshint esversion: 6 */
+
+process.env.NODE_ENV = 'test'
+
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+chai.use(chaiHttp);
+
+describe('API Routes', () => {
+
+  describe('GET routes', () => {
+
+    it('should return all text samples', () => {
+      chai.request(server)
+      .get('api/v1/text_samples')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body[0].should.have.property('title');
+        res.body[0].title.should.equal('Serenity Harbor: A Heartwarming Small Town Romance (Haven Point)');
+        res.body[0].should.have.property('body');
+        res.body[0].body.should.be.a('string');
+        res.body[0].should.have.property('id');
+        done();
+      });
+    });
+
+    it('should return all verbs', () => {
+      chai.request(server)
+      .get('api/v1/verbs')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body[0].should.have.property('word');
+        res.body[0].word.should.equal('named');
+        res.body[0].should.have.property('type');
+        res.body[0].type.should.equal('VBD');
+        res.body[0].should.have.property('id');
+        done();
+      });
+    });
+
+    it('should return all adverbs', () => {
+      chai.request(server)
+      .get('api/v1/adverbs')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body[0].should.have.property('word');
+        res.body[0].word.should.equal('little');
+        res.body[0].should.have.property('type');
+        res.body[0].type.should.equal('RB');
+        res.body[0].should.have.property('id');
+        done();
+      });
+    });
+
+    it('should return all adjectives', () => {
+      chai.request(server)
+      .get('api/v1/adjectives')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body[0].should.have.property('word');
+        res.body[0].word.should.equal('idyllic');
+        res.body[0].should.have.property('type');
+        res.body[0].type.should.equal('JJ');
+        res.body[0].should.have.property('id');
+        done();
+      });
+    });
+
+    it('should return all nouns', () => {
+      chai.request(server)
+      .get('api/v1/nouns')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body[0].should.have.property('word');
+        res.body[0].word.should.equal('Come');
+        res.body[0].should.have.property('type');
+        res.body[0].type.should.equal('NNP');
+        res.body[0].should.have.property('id');
+        done();
+      });
+    });
+
+
+  });
+});
