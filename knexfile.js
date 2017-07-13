fd8834b4bdbcde1406c9067d9058b65b6abcc0c4
Laura Turk <laura.lk.turk@gmail.com>
2017-07-13 15:56:21 -0600

    configure knexfile for deployment

--- knexfile.js
+++ knexfile.js
@@ -22,36 +22,13 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  staging: {
-    client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
   }
 
 };
